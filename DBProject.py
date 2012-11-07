d311a9ee41dee83cf360ad7da9473af682cac773
William Hill <mjh2225@gmail.com>
2012-11-07 10:43:26 -0800

    initial commit

--- DBProject.py
+++ DBProject.py
@@ -13,13 +13,31 @@ db = MySQLdb.connect('127.0.0.1', 'wmh80', 'ab1234', 'wmh80')
 
 cursor = db.cursor()
 
+selection = raw_input("Would you like to enter a book?\n")
+if selection == 'yes':
+    print "Please enter an ISBN number:"
+    ISBN = raw_input()
+    print "Please enter a title: "
+    title = raw_input()
+    cursor.execute("INSERT INTO Book(ISBN,Title) VALUES(%s, %s) ", (ISBN, title))
+    #print "book selected"
+else:
+    print "No selection"
+
+    
+
+    
+
+
+
 #cursor.execute('describe Book')
-cursor.execute("INSERT INTO Book(ISBN,Title) VALUES('2352325', 'C Programming') ")
+#cursor.execute("INSERT INTO Book(ISBN,Title) VALUES('13523251', 'C Programming 2') ")
 #cursor.execute("INSERT INTO Book(Title) VALUES('Algorithms')")
 #cursor.execute("INSERT INTO Book(Title) VALUES('New book') ")
 
-cursor.execute("SELECT * FROM Book")
+#cursor.execute("SELECT * FROM Book")
 db.commit()
+#cursor.execute('describe Book')
 #print "This is a test"
 #name = raw_input()
 #print "Hi," , name
